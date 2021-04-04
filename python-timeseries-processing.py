35aeb4b52199ff7fe6ae926a86e8e2949d8d6239
Smith <bartlomiejczajewski@gmail.com>
2021-04-04 19:46:02 +0200

    Add datetime objects section

---
+++ python-timeseries-processing.py
@@ -0,0 +1,9 @@
+
+#==================#
+# datetime objects #
+#==================#
+
+# Create datetime object from string
+import datetime
+date_time_str = '18/09/93'
+date_time_obj = datetime.datetime.strptime(date_time_str, '%d/%m/%y')
